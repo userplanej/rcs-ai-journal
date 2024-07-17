d6a1384bd2d327c4256d01aee14bc56939595c89
Lee jaehyu <jaehyu@userplane.co.kr>
2024-07-17 11:19:40 +0900

    chore : add images and icons

--- app/(stories)/about/page.tsx
+++ app/(stories)/about/page.tsx
@@ -14,6 +14,11 @@ export default async function AboutPage() {
           <p><span className="m-1">폴 그래이엄의 해커 뉴스 Site 를 Next.js 를 사용하여 복제한 것 입니다.</span></p>
           <p><span className="m-1">단 style 은 원래의 H/N site 를 그대로 가져왔지만 채워지는 내용은 LLM 에 의해 생성된 내용으로 채웠습니다 </span></p>
           <p><span className="m-1">cron 탭에 등록된 일정 시각에 Next.js 어플리케이션은 LLM -미스트랄 AI-에 새로운 기사와 함께 문맥에 어울리는 댓글을 생성하도록 요청을 합니다</span></p>
+          <div className="w-full flex items-center justify-start pl-10 py-10 pt-5">
+          <img width="640"  src="/images/prompt.png" />
+          </div>
+          
+          
           <p><span className="m-1">응답으로 돌아온 생성된 기사들과 댓글은 Next.js 로 구현된 Agentic 행위에 의해 데이터베이스에 수록이 됩니다</span></p>
 
           <p><span className="m-1">이와 더불어 slash key 를 누르시면 검색창이 활성화 되는데 아주 빠른 like 검색 결과를 확인할 수 있습니다</span></p>
